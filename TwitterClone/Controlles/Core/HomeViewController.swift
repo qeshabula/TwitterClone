3497b7634a4ba0fb597d85558431b5ce7fc763fb
Bula <umbetalyevv@gmail.com>
2023-01-12 21:39:44 +0600

    Added TweetTableViewCell

--- TwitterClone/Controlles/Core/HomeViewController.swift
+++ TwitterClone/Controlles/Core/HomeViewController.swift
@@ -11,7 +11,7 @@ class HomeViewController: UIViewController {
     
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
         return tableView
     }()
 
@@ -34,8 +34,9 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 10
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Tweet"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else {
+            return UITableViewCell()
+        }
         return cell
     }
 }
