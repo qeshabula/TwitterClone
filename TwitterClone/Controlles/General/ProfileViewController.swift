559e580b37f27e165322d4b428ae98d748ec455f
Bula <umbetalyevv@gmail.com>
2023-01-13 14:47:24 +0600

    Configuration NavBar and ProfileVC

---
+++ TwitterClone/Controlles/General/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  TwitterClone
+//
+//  Created by Bula on 1/13/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Profile"
+    }
+
+}
