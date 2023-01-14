b03eb7887023dc39b2637e4e5345497b2935280e
Bula <umbetalyevv@gmail.com>
2023-01-15 00:02:58 +0600

    Adding Networking/Combine/Create New User

---
+++ TwitterClone/Networking/AuthManager.swift
@@ -0,0 +1,22 @@
+//
+//  AuthManager.swift
+//  TwitterClone
+//
+//  Created by Bula on 1/14/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuthCombineSwift
+import Combine
+
+class AuthManager {
+    static let shared = AuthManager()
+    
+    func registerUser(with email: String, password: String) -> AnyPublisher<User, Error> {
+        return Auth.auth().createUser(withEmail: email, password: password)
+            .map(\.user)
+            .eraseToAnyPublisher()
+    }
+    
+}
